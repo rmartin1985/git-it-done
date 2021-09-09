104b645530846dedd6230213cd950a60144c1a1e
Rick Martin <rmartin1985@att.net>
2021-09-09 15:43:28 -0500

    added link if more than 30 issues exist for repo

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 
 var getRepoIssues = function (repo) {
@@ -10,6 +11,11 @@ var getRepoIssues = function (repo) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -54,4 +60,17 @@ var displayIssues = function (issues) {
 
 };
 
-getRepoIssues("rmartin1985/git-it-done");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning contanier
+    limitWarningEl.appendChild(linkEl);
+}
+
+getRepoIssues("facebook/react");
